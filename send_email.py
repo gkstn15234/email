07b126d4d0f1e12300c627a61b1db0c87b6d2356
Korean News <hangil9910@gmail.com>
2025-07-19 02:19:45 +0900

    이메일 자동 발송 시스템 재생성

--- send_email.py
+++ send_email.py
@@ -2,10 +2,12 @@ import os
 import smtplib
 from email.message import EmailMessage
 
+# GitHub Secrets를 통해 전달된 환경 변수에서 이메일 정보 가져오기
 SENDER_EMAIL = os.getenv('SENDER_EMAIL')
 SENDER_PASSWORD = os.getenv('SENDER_PASSWORD')
 RECIPIENT_EMAIL = os.getenv('RECIPIENT_EMAIL')
 
+# 이메일 내용 설정
 subject = "자동화된 이메일 알림입니다 📬"
 body = """
 안녕하세요!
@@ -16,12 +18,14 @@ body = """
 좋은 하루 보내세요!
 """
 
+# 이메일 메시지 객체 생성
 msg = EmailMessage()
 msg['Subject'] = subject
 msg['From'] = SENDER_EMAIL
 msg['To'] = RECIPIENT_EMAIL
 msg.set_content(body)
 
+# Gmail SMTP 서버에 연결하여 이메일 발송
 try:
     with smtplib.SMTP_SSL('smtp.gmail.com', 465) as smtp:
         smtp.login(SENDER_EMAIL, SENDER_PASSWORD)
